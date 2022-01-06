8d8b96081f116476edf57208f97ec049e8a0d8d7
majdi aribi <majdi.developer@gmail.com>
2022-01-06 15:42:10 +0100

    add image for flutter icon

--- lib/ui/widgets/custom_appbar.dart
+++ lib/ui/widgets/custom_appbar.dart
@@ -54,6 +54,6 @@ class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  // TODO: implement preferredSize
-  Size get preferredSize => Size.fromHeight(56);
+ 
+  Size get preferredSize =>const Size.fromHeight(56);
 }
